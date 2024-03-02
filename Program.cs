ded3fe20ff7e742cc91e28b17fcc955c35b7b06f
Szymon K <s-koniecko@wp.pl>
2024-03-02 22:06:22 +0100

    builded args

--- Program.cs
+++ Program.cs
@@ -1,20 +1,39 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using TestFrameworkComparison.Helpers;
+using TestFrameworkComparison.Services;
 using TestFrameworkComparison.Services.Sort;
 
 namespace TestFrameworkComparison
 {
     internal class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            var serviceProvider = new ServiceCollection()
-                .AddScoped<ISortService, QuickSortService>()
-                .AddScoped<SortService>()
-                .BuildServiceProvider();
-
-            var sortingService = serviceProvider.GetRequiredService<SortService>();
-
-            sortingService.ExecuteSort(0);
+            if (args.Length == 0)
+            {
+                throw new ArgumentNullException("MISSING ARGUMENTS");
+            }
+            int index = 0;
+            var serviceProvider = new ServiceCollection();
+            int fileIndex = 0;
+            foreach (var param in args)
+            {
+                if (!string.IsNullOrEmpty(param))
+                {
+                    if (param.Equals("--Sort"))
+                    {
+                        serviceProvider.GetSortService(args[index + 1]);
+                        fileIndex = index + 2;
+                    }
+                }
+                index++;
+            }
+            PrinterHelper.PrintNumbers(args);
+            if (args.Contains("--Sort"))
+            {
+                var sortingService = serviceProvider.AddScoped<SortService>().BuildServiceProvider().GetService<SortService>();
+                sortingService.ExecuteSort(int.Parse(args[fileIndex]));
+            }
         }
     }
 }
