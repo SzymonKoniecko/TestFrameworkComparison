b51aa5e7c42e5c03217dbaf26cefdeb716edccf0
Szymon K <s-koniecko@wp.pl>
2024-03-08 19:37:31 +0100

    init

---
+++ MsTestProject/Models/PerformanceMeterDto.cs
@@ -0,0 +1,25 @@
+﻿namespace MsTestProject.Models
+{
+    public class PerformanceMeterDto
+    {
+        private static Stopwatch _stopwatch = new Stopwatch();
+        public long StartMemory { get; private set; }
+        public long EndMemory { get; private set; }
+        public TimeSpan Elapsed { get; private set; }
+        public FrameworkEnum Framework { get; private set; } = FrameworkEnum.XUnit;
+        public void StartTest()
+        {
+            _stopwatch.Start();
+            StartMemory = GC.GetTotalMemory(true);
+        }
+        public string EndTest()
+        {
+            _stopwatch.Stop();
+            EndMemory = GC.GetTotalMemory(true);
+            Elapsed = _stopwatch.Elapsed;
+            return new string("#PERFORMANCE_DATA_START " +
+                JsonConvert.SerializeObject(this) +
+                " #PERFORMANCE_DATA_END");
+        }
+    }
+}
